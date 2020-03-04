3690993fd7ddb177e0b4a0ba1be2104d326ea174
Ahmed Sami <ahmed.sami@robustastudio.com>
2020-03-04 16:53:30 +0200

    feat: updated getOrders function.
    Now accepts an object of 4 keys:
    limit: number, default 20
    page: number, default 1,
    status: string, 'open' | 'issued' | 'cancelled' | 'billed' | 'closed'
    purchaseorderNumber: string

--- src/lib/products.js
+++ src/lib/products.js
@@ -141,14 +141,31 @@ class Products extends Request {
   /**
    *  Get all current orders
    *
-   * @param {number} [limit=25]
-   * @param {number} [page=1]
+   * @param {object} {
+   *     limit {number} [ 10 .. 1000 ] characters
+   *     page {number} non-empty,
+   *     statu {enum} = open/ issued/ cancelled/ billed/ closed,
+   *     purchaseorderNumber {string}
+   *   }
    * @returns
    * @see https://knawat-suppliers.restlet.io/#operation_get_order_s_
    * @memberof Products
    */
-  getOrders(limit = 25, page = 1) {
-    return this.$fetch('GET', '/orders', { queryParams: { limit, page } });
+  getOrders({
+    limit = 20,
+    page = 1,
+    status = null,
+    purchaseorderNumber = null
+  } = {}) {
+    // URL query params
+    const queryParams = {
+      page,
+      limit,
+      status,
+      purchaseorderNumber
+    };
+
+    return this.$fetch('GET', '/orders', { queryParams });
   }
 
   /**
